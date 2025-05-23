8644b60c814c55efc4a86dfc1d5ee9bb299513c0
Pietro Romano <pietro.romano.tech@gmail.com>
2025-05-23 18:21:21 -0300

    Ajustes nas colisões e na física para simulação de quicar e de fricção dos personagens e da bola - Mecânica inicial de arremesar o jogador ao contrário da posição do mouse atual no mundo 3D e com base no tempo que o botão esquerdo do mouse está segundo segurado aumenta a força que o jogador será arremesado.

--- Features/Player/Scripts/Player.gd
+++ Features/Player/Scripts/Player.gd
@@ -1,20 +1,49 @@
-extends Node3D
+extends RigidBody3D
 
 @onready var camera = $"../Camera3D"
+@onready var direction_and_force_bar = $"../SubViewport/ProgressBar"
+@onready var direction_and_force_bar_mesh_pivot = $"../ArrowPivot"
+
+var is_holding: bool = false
+var force_increase_factor: float = 50.0
+var last_mouse_world_direction: Vector3 = Vector3.ZERO
 
 func _input(event: InputEvent):
-	if event is InputEventMouseButton:		
-		if  event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:		
+		match event:
+			_ when event.is_pressed():
+				var result: Dictionary = get_mouse_3D_info(event.position)
+				if result and result.collider and result.collider.name == "Player":
+					print("crico no preier")
+					is_holding = true
+
+			_ when event.is_released():
+				print("sorto o crique")
+				is_holding = false
+
+func _process(delta: float) -> void:
+	direction_and_force_bar_mesh_pivot.global_position = global_position
+	if is_holding:		
+		direction_and_force_bar.value += force_increase_factor * delta
+		var result: Dictionary = get_mouse_3D_info(get_viewport().get_mouse_position())
+		if result and result.collider:
+			var direction_to_mouse: Vector3 = (result.position - global_position).normalized()
+			direction_to_mouse.y = 0.0
+			last_mouse_world_direction = direction_to_mouse
+			var angle: float = atan2(direction_to_mouse.x, direction_to_mouse.z)
 			
-			var from: Vector3 = camera.project_ray_origin(event.position);
-			var to: Vector3 = from + camera.project_ray_normal(event.position) * 1000
-			
-			var space_state: PhysicsDirectSpaceState3D = get_world_3d().direct_space_state
-			var query := PhysicsRayQueryParameters3D.create(from, to)
-			query.collide_with_areas = true
-			query.collide_with_bodies = true
-			
-			var result: Dictionary = space_state.intersect_ray(query)
-			
-			if result and result.collider:
-				pass
+			direction_and_force_bar_mesh_pivot.rotation.y = angle		
+	else:
+		apply_central_impulse(last_mouse_world_direction * -direction_and_force_bar.value)
+		direction_and_force_bar.value = 0.0
+		
+func get_mouse_3D_info(mouse_position: Vector2) -> Dictionary:
+	var from: Vector3 = camera.project_ray_origin(mouse_position)
+	var to: Vector3 = from + camera.project_ray_normal(mouse_position) * 1000
+
+	var space_state: PhysicsDirectSpaceState3D = get_world_3d().direct_space_state
+	var query := PhysicsRayQueryParameters3D.create(from, to)
+	query.collide_with_areas = true
+	query.collide_with_bodies = true
+
+	return space_state.intersect_ray(query)
