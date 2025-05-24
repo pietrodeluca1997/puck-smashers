9d87008182788fdd87f487cc5d88b4cb448bad15
Pietro Romano <pietro.romano.tech@gmail.com>
2025-05-24 13:34:03 -0300

    Melhoria na física e nas colisões dos RigidBodies - Filtro de máscara de colisão para permitir uma colisão específica para o mouse para uma melhor experiência de jogo.

--- Features/Player/Scripts/Player.gd
+++ Features/Player/Scripts/Player.gd
@@ -1,20 +1,24 @@
 extends RigidBody3D
 
 @onready var camera = $"../Camera3D"
-@onready var direction_and_force_bar = $"../SubViewport/ProgressBar"
-@onready var direction_and_force_bar_mesh_pivot = $"../ArrowPivot"
+@onready var direction_and_force_bar = $"../HUD/SubViewport/ProgressBar"
+@onready var direction_and_force_bar_mesh_pivot = $"../HUD/ArrowPivot"
 
 var is_holding: bool = false
 var force_increase_factor: float = 50.0
 var last_mouse_world_direction: Vector3 = Vector3.ZERO
 
+func _ready():
+	direction_and_force_bar_mesh_pivot.visible = false
+
 func _input(event: InputEvent):
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:		
 		match event:
 			_ when event.is_pressed():
-				var result: Dictionary = get_mouse_3D_info(event.position)
-				if result and result.collider and result.collider.name == "Player":
+				var result: Dictionary = get_mouse_3D_info(event.position, 2)
+				if result and result.collider and result.collider.name == "PlayerInteractArea":
 					print("crico no preier")
+					direction_and_force_bar_mesh_pivot.visible = true
 					is_holding = true
 
 			_ when event.is_released():
@@ -22,10 +26,10 @@ func _input(event: InputEvent):
 				is_holding = false
 
 func _process(delta: float) -> void:
-	direction_and_force_bar_mesh_pivot.global_position = global_position
+	direction_and_force_bar_mesh_pivot.global_position = Vector3(global_position.x, direction_and_force_bar_mesh_pivot.global_position.y, global_position.z)
 	if is_holding:		
 		direction_and_force_bar.value += force_increase_factor * delta
-		var result: Dictionary = get_mouse_3D_info(get_viewport().get_mouse_position())
+		var result: Dictionary = get_mouse_3D_info(get_viewport().get_mouse_position(), 1)
 		if result and result.collider:
 			var direction_to_mouse: Vector3 = (result.position - global_position).normalized()
 			direction_to_mouse.y = 0.0
@@ -34,10 +38,11 @@ func _process(delta: float) -> void:
 			
 			direction_and_force_bar_mesh_pivot.rotation.y = angle		
 	else:
+		direction_and_force_bar_mesh_pivot.visible = false
 		apply_central_impulse(last_mouse_world_direction * -direction_and_force_bar.value)
 		direction_and_force_bar.value = 0.0
 		
-func get_mouse_3D_info(mouse_position: Vector2) -> Dictionary:
+func get_mouse_3D_info(mouse_position: Vector2, mask: int) -> Dictionary:
 	var from: Vector3 = camera.project_ray_origin(mouse_position)
 	var to: Vector3 = from + camera.project_ray_normal(mouse_position) * 1000
 
@@ -45,5 +50,14 @@ func get_mouse_3D_info(mouse_position: Vector2) -> Dictionary:
 	var query := PhysicsRayQueryParameters3D.create(from, to)
 	query.collide_with_areas = true
 	query.collide_with_bodies = true
+	query.collision_mask = mask
 
 	return space_state.intersect_ray(query)
+
+
+func _on_left_goal_body_entered(body: Node3D) -> void:	
+	pass
+
+
+func _on_right_goal_body_entered(body: Node3D) -> void:
+	pass
